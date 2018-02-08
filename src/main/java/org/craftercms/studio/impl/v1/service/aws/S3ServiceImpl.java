20c4cb52d732285535fe014ee76dbc4f86f69f97
jdrossl <jdross17@gmail.com>
2018-02-08 16:50:55 -0600

    Added rest script to list files in an aws bucket

--- src/main/java/org/craftercms/studio/impl/v1/service/aws/S3ServiceImpl.java
+++ src/main/java/org/craftercms/studio/impl/v1/service/aws/S3ServiceImpl.java
@@ -1,6 +1,10 @@
 package org.craftercms.studio.impl.v1.service.aws;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.craftercms.commons.validation.annotations.param.ValidateStringParam;
 import org.craftercms.studio.api.v1.aws.s3.S3Output;
@@ -11,6 +15,8 @@ import org.craftercms.studio.api.v1.service.aws.S3Service;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 /**
  * Default implementation of {@link S3Service}.
@@ -54,4 +60,24 @@ public class S3ServiceImpl extends AbstractAwsService<S3Profile> implements S3Se
         return output;
     }
 
+    @Override
+    public Map listFiles(final String site, final String profileId) throws AwsException {
+        S3Profile profile = getProfile(site, profileId);
+        AmazonS3 s3Client = getS3Client(profile);
+        String bucketName = profile.getBucketName();
+
+        ListObjectsV2Result result = s3Client.listObjectsV2(bucketName);
+        List<S3ObjectSummary> objects = result.getObjectSummaries();
+        Map toRet = new HashMap();
+        List<String> keys = new ArrayList<>(objects.size());
+
+        if(!objects.isEmpty()) {
+            objects.stream().filter(object -> object.getSize() > 0).forEach(object -> keys.add(object.getKey()));
+        }
+
+        toRet.put("bucket", bucketName);
+        toRet.put("keys", keys);
+
+        return toRet;
+    }
 }
